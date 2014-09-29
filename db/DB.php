3d0611c896565060be1c6b55d7f1513f99a64c9e
usuario <usuario@192.168.3.114>
2014-09-29 12:34:38 +0200

    Front Controller Updated

--- db/DB.php
+++ db/DB.php
@@ -2,10 +2,10 @@
 
 abstract class DB {
 
-    const DBHOST = "localhost";
-    const DBUSER = "root";
-    const DBPASS = "test";
-    const DBNAME = "nocturno";
+    const DBHOST = "";
+    const DBUSER = "";
+    const DBPASS = "";
+    const DBNAME = "";
 
     protected $conn;
 
